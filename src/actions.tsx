e2a8d78cd8e245d81ed8cd05d230b3c5276e2acd
yushever <iushina.tatiana@gmail.comgit config --global user.email iushina.tatiana@gmail.comgit config --global user.email iushina.tatiana@gmail.com>
2022-11-21 17:17:03 +0300

    const

--- src/actions.tsx
+++ src/actions.tsx
@@ -2,22 +2,34 @@ import GetTickets from './components/services/service';
 
 const ticketsService = new GetTickets();
 
-export const checkAll = () => ({ type: 'ALL' });
-export const checkDirect = () => ({ type: 'DIRECT' });
-export const checkOne = () => ({ type: '1LAYOVER' });
-export const checkTwo = () => ({ type: '2LAYOVER' });
-export const checkThree = () => ({ type: '3LAYOVER' });
-export const chooseCheapest = () => ({ type: 'CHEAP' });
-export const chooseFastest = () => ({ type: 'FAST' });
-export const chooseOptimal = () => ({ type: 'OPTIMAL' });
-export const showMoreTickets = () => ({ type: '5MORE' });
+export const ALL = 'ALL';
+export const DIRECT = 'DIRECT';
+export const LAYOVER1 = '1LAYOVER';
+export const LAYOVER2 = '2LAYOVER';
+export const LAYOVER3 = '3LAYOVER';
+export const CHEAP = 'CHEAP';
+export const FAST = 'FAST';
+export const OPTIMAL = 'OPTIMAL';
+export const MORE5 = '5MORE';
+export const SET = 'SET';
+export const ALLSET = 'ALLSET';
+
+export const checkAll = () => ({ type: ALL });
+export const checkDirect = () => ({ type: DIRECT });
+export const checkOne = () => ({ type: LAYOVER1 });
+export const checkTwo = () => ({ type: LAYOVER2 });
+export const checkThree = () => ({ type: LAYOVER3 });
+export const chooseCheapest = () => ({ type: CHEAP });
+export const chooseFastest = () => ({ type: FAST });
+export const chooseOptimal = () => ({ type: OPTIMAL });
+export const showMoreTickets = () => ({ type: MORE5 });
 export const setTickets = (tickets: { tickets: [] }) => {
   return {
-    type: 'SET',
+    type: SET,
     payload: tickets.tickets,
   };
 };
-export const setAllTickets = () => ({ type: 'ALLSET' });
+export const setAllTickets = () => ({ type: ALLSET });
 
 async function showAllTickets(dispatch: any) {
   let tickets = await ticketsService.getAllTickets();
--- src/reducer.tsx
+++ src/reducer.tsx
@@ -1,6 +1,7 @@
 import { Reducer } from 'redux';
 
 import { IState } from './models';
+import * as actions from './actions';
 
 const reducer: Reducer = (
   state: IState = {
@@ -17,42 +18,42 @@ const reducer: Reducer = (
   action: { type?: string; payload?: any } = {}
 ) => {
   switch (action.type) {
-    case 'ALL':
+    case actions.ALL:
       if (state.all === true) {
         return { ...state, all: false, direct: false, layover1: false, layover2: false, layover3: false };
       }
       return { ...state, all: true, direct: true, layover1: true, layover2: true, layover3: true };
-    case 'DIRECT':
+    case actions.DIRECT:
       if (state.direct === true) {
         return { ...state, direct: false, all: false };
       }
       return { ...state, direct: true, all: state.layover1 && state.layover2 && state.layover3 };
-    case '1LAYOVER':
+    case actions.LAYOVER1:
       if (state.layover1 === true) {
         return { ...state, layover1: false, all: false };
       }
       return { ...state, layover1: true, all: state.direct && state.layover2 && state.layover3 };
-    case '2LAYOVER':
+    case actions.LAYOVER2:
       if (state.layover2 === true) {
         return { ...state, layover2: false, all: false };
       }
       return { ...state, layover2: true, all: state.layover1 && state.direct && state.layover3 };
-    case '3LAYOVER':
+    case actions.LAYOVER3:
       if (state.layover3 === true) {
         return { ...state, layover3: false, all: false };
       }
       return { ...state, layover3: true, all: state.layover1 && state.layover2 && state.direct };
-    case 'CHEAP':
+    case actions.CHEAP:
       return { ...state, filter: 'cheapest' };
-    case 'FAST':
+    case actions.FAST:
       return { ...state, filter: 'fastest' };
-    case 'OPTIMAL':
+    case actions.OPTIMAL:
       return { ...state, filter: 'optimal' };
-    case '5MORE':
+    case actions.MORE5:
       return { ...state, number: state.number + 5 };
-    case 'SET':
+    case actions.SET:
       return { ...state, tickets: [...state.tickets, ...action.payload] };
-    case 'ALLSET':
+    case actions.ALLSET:
       return { ...state, loading: false };
 
     default:
